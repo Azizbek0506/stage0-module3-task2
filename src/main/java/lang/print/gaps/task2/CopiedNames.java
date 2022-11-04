b58fdca27f8ebe13b4f103ae33c034e17b7d5c46
i7 <azizbeko5o6o2@gmail.com>
2022-11-04 17:29:28 +0500

    Merge remote-tracking branch 'origin/main'

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,20 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        int variable1 = 2000;
         int value = 1000;
-        int variable = 4000;
+        int variable3 = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        int value = 90000;
+        int value3 = 90000;
+        System.out.println(variable);
+        System.out.println(variable1);
+        System.out.println(value);
+        System.out.println(variable3);
+        System.out.println(value2);
+        System.out.println(variables);
+        System.out.println(value3);
+
     }
 }
